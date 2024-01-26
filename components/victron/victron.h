ad83c49657579f277bcfdb025a188b7042484bfb
Yannik <greepee1337@gmail.com>
2024-01-26 11:03:12 +0100

    Update victron.h

--- components/victron/victron.h
+++ components/victron/victron.h
@@ -291,7 +291,7 @@ class VictronComponent : public uart::UARTDevice, public Component {
   uint32_t last_transmission_{0};
   uint32_t last_publish_{0};
   uint32_t throttle_{0};
-  bool block_input_{false];
+  bool block_input_{false};
 };
 
 }  // namespace victron
